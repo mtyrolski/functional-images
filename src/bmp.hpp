c6e694104919dcfad8b710e64037076f6278596e
mvxxx <michal.tyrolski@gmail.com>
2020-02-09 16:40:26 +0100

    example

--- src/bmp.hpp
+++ src/bmp.hpp
@@ -3,9 +3,8 @@
 
 #include "images.hpp"
 #include <cstdint>
-#include <string>
 
-void create_BMP(const std::string &filename, const uint32_t width,
-                const uint32_t height, const Image &f);
+void create_BMP(const std::string &filename, uint32_t width,
+                uint32_t height, const Image &f);
 
 #endif
